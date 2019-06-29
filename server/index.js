6a8c54c8842b0399745d55179e84946d300fa746
Harrison Wu <harrisonywu@gmail.com>
2019-06-29 16:13:57 -0700

    Create connection using Socket.io

--- server/index.js
+++ server/index.js
@@ -1,12 +1,28 @@
-const express = require('express');
-const morgan = require('morgan');
 const path = require('path');
+var express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var socket = require('socket.io-client')('http://localhost:3000');
 
-app.use(morgan('dev'));
-
+server.listen(3000);
 app.use(express.static(path.join(__dirname, '../client/dist')));
+// WARNING: app.listen(80) will NOT work here!
 
 
-app.listen(port, console.log(`listening on port ${port}`));
+socket.on('connect', () => {
+  console.log('yo')
+})
+
+io.on('connection', function (socket) {
+  console.log('hi')
+  io.emit('this', { will: 'be received by everyone'});
+
+  socket.on('private message', function (from, msg) {
+    console.log('I received a private message by ', from, ' saying ', msg);
+  });
+
+  socket.on('disconnect', function () {
+    io.emit('user disconnected');
+  });
+});
